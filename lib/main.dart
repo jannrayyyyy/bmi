d44791c5e656c981f19a6622df0b0f3e667c4e08
Jannray Mostajo <102408878+jannrayyyyy@users.noreply.github.com>
2022-06-17 23:52:59 -0700

    commit day 1.0.2

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'BMI',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.blueGrey),
       home: HomePage(),
